1556e4b0f0227b499a076c8cd9b2a1b2c5c2a244
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2022-07-10 11:30:00 +0530

    Create Q1. Multiplication of previous and next.java

---
+++ 0010 - Arrays & Dynamic Arrays/Q1. Multiplication of previous and next.java
@@ -0,0 +1,61 @@
+Given an array of integers A, update every element with multiplication of previous and next elements with following exceptions. a) First element is replaced by multiplication of first and second. b) Last element is replaced by multiplication of last and second last.
+
+
+Input Format
+
+The only argument given is the integer array A.
+Output Format
+
+Return the updated array.
+Constraints
+
+1 <= length of the array <= 100000
+-10^4 <= A[i] <= 10^4 
+For Example
+
+Input 1:
+    A = [1, 2, 3, 4, 5]
+Output 1:
+    [2, 3, 8, 15, 20]
+
+Input 2:
+    A = [5, 17, 100, 11]
+Output 2:
+    [85, 500, 187, 1100]
+    
+
+
+
+
+
+
+
+public class Solution {
+    public int[] solve(int[] A) {
+
+        int n=A.length;
+
+        if(n<2)return A;
+
+        int []arr=new int[n];
+
+        for(int i=0;i<n;i++){
+
+            if(i==0){
+                arr[i]=A[i]*A[i+1];
+            }
+
+            else if(i==n-1){
+                arr[i]=A[i]*A[i-1];
+            }
+            else{
+                arr[i]=A[i+1]*A[i-1];
+            }
+        }
+
+        return arr;
+    }
+}
+
+
+
