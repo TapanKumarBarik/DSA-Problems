04e1a6092e8cddad860fda1fe422befb0bc4cbc3
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2022-07-12 21:05:33 +0530

    Update Q2. Concatenate Three Numbers.java

--- 0016 - Arrays & Maths/Q2. Concatenate Three Numbers.java
+++ 0016 - Arrays & Maths/Q2. Concatenate Three Numbers.java
@@ -66,3 +66,25 @@ Explanation 2:
         return 10000*a[0] + 100*a[1] + a[2];
     }
 }
+
+
+//*************************************************************************************
+
+public class Solution {
+    public int solve(int A, int B, int C) {
+        
+        String s= "";
+        
+   int []arr=new int[3];
+   arr[0]=A;
+   arr[1]=B;
+   arr[2]=C;
+   Arrays.sort(arr);
+   s+=arr[0];
+   s+=arr[1];
+   s+=arr[2];
+ 
+        
+        return Integer.parseInt(s);
+    }
+}
